950e58d2b428876bffcf2974a801a277da167f06
DelpinSusaiRaj <susairajs@outlook.com>
2017-02-26 17:22:40 +0530

    add all files

---
+++ XamarinProgressBar/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinProgressBar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await MainProgressBar.ProgressTo(0.8, 1000, Easing.Linear);
+        }
+    }
+}
